c8bee802f978d964a6a0612edd51faeaea34c397
Pithou95 <timothee.kim03@gmail.com>
2026-05-15 14:14:44 +0200

    Update code_match_Falla_arduino_ide.ino

--- code_match_Falla_arduino_ide.ino
+++ code_match_Falla_arduino_ide.ino
@@ -335,6 +335,14 @@ void loop() {
                 dec2 = 200;
                 dec3 = 250;    //A CHANGER AVANT MATCH
             }
+
+             //Pour verifier que la tirette est bien mise
+            if (digitalRead(tirette) == LOW){  
+                digitalWrite(2, HIGH);
+                delay(50);
+                digitalWrite(2,LOW);
+            } 
+         
             if (temps > 0 && millis() - temps >= delai){
                 pulseCount_D = 0; pulseCount_G = 0;
                 prev_pulses_D = 0; prev_pulses_G = 0;
